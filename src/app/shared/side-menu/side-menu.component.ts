6ea45420164870ec01a38dada1bbdfaf3fc451ad
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-11-24 07:15:54 -0600

    config sidemenu

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { routes } from '../../app.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.scss'
+  styleUrl: './side-menu.component.scss',
 })
 export class SideMenuComponent {
-
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes('**'))
+    .filter((route) => !route.path?.includes(':'));
 }
