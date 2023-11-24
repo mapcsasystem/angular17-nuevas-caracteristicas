dfe96d16efcc14a7952a658080a26cf262324d7f
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-11-24 10:31:51 -0600

    changeDetection: ChangeDetectionStrategy.OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection.component.html',
-  styleUrl: './change-detection.component.scss'
+  styleUrl: './change-detection.component.scss',
 })
 export class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change Detection ${this.frameworkAsSignal().name}`
+  );
 
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsSignal.update((value) => ({ ...value, name: 'React' }));
+      // this.frameworkAsProperty.name = 'React';
+      console.log('Hecho');
+    }, 3000);
+  }
 }
