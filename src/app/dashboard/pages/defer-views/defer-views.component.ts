4e7c4a216c1cda30b072f6cddf8101c3b1282005
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-11-25 09:42:29 -0600

    @defer () @placeholder

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeavyLoadersSlowComponent } from '../../../shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
-  styleUrl: './defer-views.component.scss'
+  styleUrl: './defer-views.component.scss',
 })
-export class DeferViewsComponent {
-
-}
+export class DeferViewsComponent {}
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
   imports: [CommonModule],
-  template: `<h1>Hola mundo</h1>`,
+  template: ` <section [ngClass]="['w-full h-[600px]', cssClass]">
+    Heavy Loader slow
+  </section>`,
 })
-export class HeavyLoadersSlowComponent {}
+export class HeavyLoadersSlowComponent {
+  @Input({ required: true }) cssClass!: string;
+  constructor() {
+    const start = Date.now();
+    while (Date.now() - start < 3000) {}
+  }
+}
