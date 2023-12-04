f45bc12db832963aa953c5c52de47941a9bfa150
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-12-04 14:23:22 -0600

    Show List Users

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, signal } from '@angular/core';
-import { User } from '@interfaces/req-response';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { User, UsersResponse } from '@interfaces/req-response';
+import { delay } from 'rxjs';
 
 interface State {
   users: User[];
@@ -11,12 +13,25 @@ interface State {
 })
 export class UserService {
   // TODO el caracter # es para poner el state en privado directamente en javascript
+  #http = inject(HttpClient);
+
   #state = signal<State>({
     users: [],
     loading: true,
   });
 
+  public users = computed(() => this.#state().users);
+  public loading = computed(() => this.#state().loading);
+
   constructor() {
-    console.log('Cargando data');
+    this.#http
+      .get<UsersResponse>('https://reqres.in/api/users')
+      .pipe(delay(3000))
+      .subscribe((res) => {
+        this.#state.set({
+          users: res.data,
+          loading: false,
+        });
+      });
   }
 }
